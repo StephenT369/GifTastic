677f4cddd411665bf587c05e5727fbae3c11e0fd
Stephen Thompson <smt3@hotmail.com>
2019-07-19 19:48:50 -0500

    added buttons

--- assets/javascript/gifTastic.js
+++ assets/javascript/gifTastic.js
@@ -1,16 +1,15 @@
 var topics = ['geometric','galaxy space','sacred geometry','polyhedron', 'lasers', 'space travel'];
 
 $(window).on('load', function() {
-
     
     for (i = 0; i < topics.length; i++){
-
         var iTopic = topics[i];
-
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + iTopic + "&api_key=8gvZ9zZfOKVehtKyGxFv7q1RUOTqX9Ri";
+        var limit = 10;
+        var apiKey = '8gvZ9zZfOKVehtKyGxFv7q1RUOTqX9Ri';
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ iTopic +"&api_key="+ apiKey +"&limit="+ limit +"";
         var buttonText = iTopic;
-        console.log(buttonText);
         var button = $('<button>').text(buttonText);
+        
         $(button).appendTo('#buttons');    
         button.attr('id','giph-buttons');
        
@@ -20,11 +19,7 @@ $(window).on('load', function() {
         })
         .then(function(giphyResponse) {
             var giphs = giphyResponse.data;
-            console.log(giphs);
-            
-            
-            
-            
+            console.log(giphs);   
         })
     }
 });
