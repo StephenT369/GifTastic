74b406824419a0fd1b2dd4d78f912ab1a079c10c
Stephen Thompson <smt3@hotmail.com>
2019-07-21 16:32:57 -0500

    add topic button function

--- assets/javascript/gifTastic.js
+++ assets/javascript/gifTastic.js
@@ -5,53 +5,66 @@ var apiKey = '8gvZ9zZfOKVehtKyGxFv7q1RUOTqX9Ri';
 
 //Function to make buttons
 function mkButtons(){
+    $('#buttons').empty();
     for (i = 0; i < topics.length; i++){
          var buttonText = topics[i];
          var button = $('<button>').text(buttonText);
          $(button).appendTo('#buttons');
          button.attr('id', 'giph-button');    
          button.attr('topic', buttonText);
-        }
+         
+        };
+        console.log(topics);
 };
 
-//Window load event
-$(window).on('load', function() {
+
+//Display Pics
+function displayPics(){
+    var giphTopic = $(this).attr('topic');
+    $('#giphs').empty();
+    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ 
+                    giphTopic +"&api_key="+ 
+                    apiKey +"&limit="+ 
+                    limit +"";
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        })
+            .then(function(giphyResponse) {
+                var giphs = giphyResponse.data;
+                console.log(giphs);
+                for(i = 0; i < giphs.length; i++){
+                var giphDiv = $('<div>').addClass('column');
+                var giphSpan = $('<span>');
+                var rating = giphs[i].rating;
+                var brTag = $('<br>')
+                var spanTag = $('<span>').html('Rating: ' + rating + '<br><br>').addClass('rating');
+                var showImage = $('<img>');
+                showImage.attr('src', giphs[i].images.fixed_height_still.url);
+                giphDiv.prepend(brTag, spanTag);
+
+                giphDiv.append(showImage);
+                
+                $('#giphs').prepend(giphDiv);
+                };
+            });
+};
+
+$('#add-button').on('click', function(event) {
+    event.preventDefault();
+
+    var newButton = $('#topic-input').val().trim();
+    topics.push(newButton);
     mkButtons();
-
-    $('button').on('click', function(){
-        
-        var giphTopic = $(this).attr('topic');
-        $('#giphs').empty();
-        
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ 
-                        giphTopic +"&api_key="+ 
-                        apiKey +"&limit="+ 
-                        limit +"";
-        console.log(queryURL);
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-            })
-                .then(function(giphyResponse) {
-                    var giphs = giphyResponse.data;
-                    console.log(giphs);
-                    for(i = 0; i < giphs.length; i++){
-                    var giphDiv = $('<div>').addClass('column');
-                    var giphSpan = $('<span>');
-                    var rating = giphs[i].rating;
-                    var brTag = $('<br>')
-                    var spanTag = $('<span>').html('Rating: ' + rating + '<br><br>').addClass('rating');
-                    var showImage = $('<img>');
-                    showImage.attr('src', giphs[i].images.fixed_height_still.url);
-                    giphDiv.prepend(brTag, spanTag);
-
-                    giphDiv.append(showImage);
-                    
-                    $('#giphs').prepend(giphDiv);
-                    };
-                });
 });
-    });
+
+$(document).on('click', '#giph-button', displayPics);
+
+mkButtons();
+
+
 
 
 
