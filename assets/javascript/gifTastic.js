b3e7797bc4370c1c26c083e84832bd76fa2b0ef4
Stephen Thompson <smt3@hotmail.com>
2019-07-21 10:34:31 -0500

    updated buttons

--- assets/javascript/gifTastic.js
+++ assets/javascript/gifTastic.js
@@ -1,25 +1,52 @@
+//Global variables
 var topics = ['geometric','galaxy space','sacred geometry','polyhedron', 'lasers', 'space travel'];
+var iTopic;
+var limit = 10;
+var apiKey = '8gvZ9zZfOKVehtKyGxFv7q1RUOTqX9Ri';
+var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ iTopic +"&api_key="+ apiKey +"&limit="+ limit +"";
 
-$(window).on('load', function() {
-    
+//Function to make buttons
+function mkButtons(){
     for (i = 0; i < topics.length; i++){
-        var iTopic = topics[i];
-        var limit = 10;
-        var apiKey = '8gvZ9zZfOKVehtKyGxFv7q1RUOTqX9Ri';
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ iTopic +"&api_key="+ apiKey +"&limit="+ limit +"";
-        var buttonText = iTopic;
-        var button = $('<button>').text(buttonText);
-        
-        $(button).appendTo('#buttons');    
-        button.attr('id','giph-buttons');
-       
+         iTopic = topics[i];  
+    
+         var buttonText = iTopic;
+         var button = $('<button>').text(buttonText);
+         
+         $(button).appendTo('#buttons');    
+         button.attr('id','giph-buttons');
+        }
+};
+
+//Window load event
+$(window).on('load', function() {
+    mkButtons();
+
+     $('button').on('click', function(){
+
         $.ajax({
-            type: "GET",
-            url: queryURL
-        })
-        .then(function(giphyResponse) {
-            var giphs = giphyResponse.data;
-            console.log(giphs);   
-        })
-    }
-});
+            url: queryURL,
+            method: "GET"
+            })
+                .then(function(giphyResponse) {
+                    
+                    var giphs = giphyResponse.data;
+                    console.log(giphs);
+                    for(i = 0; i < giphs.length; i++){
+                    var giphDiv = $('<div>');
+                    var rating = giphs[i].rating;
+                    var pTag = $('<p>').text('Rating: ' + rating);
+                    var showImage = $('<img>');
+                    showImage.attr('src', giphs[i].images.fixed_height.url);
+                    giphDiv.prepend(pTag);
+                    giphDiv.prepend(showImage);
+                    $('#giphs').prepend(giphDiv);
+               };
+                
+                });
+    });
+    });
+
+
+
+       
